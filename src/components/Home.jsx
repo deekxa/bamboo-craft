54c2be661a89eb800ca35b8e49434b0bcd0d58c7
deekxa <dikshyak88@gmail.com>
2024-08-15 21:28:40 +0545

    Bamboo-craft

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import FeaturedSection from "./FeaturedSection"
+import Navbar from "./navbar/Navbar"
+
+const HomePage=()=>{
+    return(
+        <>
+        <Navbar/>
+        <FeaturedSection/>
+        </>
+    )
+
+}
+export default HomePage
