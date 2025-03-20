4c45b985dda0aaacc89b6c8a564acfa8bf9fd40f
deekxa <dikshyak88@gmail.com>
2025-03-20 18:02:22 +0545

    fdsajfsda

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 export const API = axios.create({
-  baseURL: "https://bamboo-backend.vercel.app/backend",
-  // baseURL: "http://localhost:4000/backend",
+  // baseURL: "https://bamboo-backend.vercel.app/backend",
+  baseURL: "http://localhost:4000/backend",
 
 });
 
